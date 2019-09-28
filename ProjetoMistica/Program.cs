7bf372e40d055bc061fa4a483f4e9f692a4eea85
EduSTR <dudustrx@gmail.com>
2019-09-28 18:35:52 -0300

    Poli-Exemplo

---
+++ ProjetoMistica/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoMistica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Veiculo[] v = new Veiculo[2];
+            v[0] = new Automovel("Ferrari");
+            v[1] = new Aeronave("Tucano");
+
+            Console.WriteLine(v[0].Tipo);
+            v[0].Mover();
+
+            Console.WriteLine(v[1].Tipo);
+            v[1].Parar();
+
+
+        }
+    }
+}
